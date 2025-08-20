afc6f8c1add9e3459835cd9336f57e40868ae0d3
Gopal Mahajan <gopal.mahajan@datakaveri.org>
2025-08-20 18:12:40 +0530

    multi-auth-added

--- src/main/java/org/cdpg/dx/auth/authentication/handler/AAAJwtAuthHandler.java
+++ src/main/java/org/cdpg/dx/auth/authentication/handler/AAAJwtAuthHandler.java
@@ -8,12 +8,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.cdpg.dx.auth.authentication.util.BearerTokenExtractor;
 
-public record KeycloakJwtAuthHandler(JWTAuth jwtAuth) implements AuthenticationHandler {
-    private static final Logger LOGGER = LogManager.getLogger(KeycloakJwtAuthHandler.class);
+public class AAAJwtAuthHandler implements AuthenticationHandler {
+    private static final Logger LOGGER = LogManager.getLogger(AAAJwtAuthHandler.class);
+    private final JWTAuth jwtAuth;
+
+    public AAAJwtAuthHandler(JWTAuth jwtAuth) {
+        this.jwtAuth = jwtAuth;
+    }
 
     @Override
     public void handle(RoutingContext ctx) {
-        LOGGER.debug("Handling authentication for Keycloak JWT");
+        LOGGER.debug("Handling authentication for AAA JWT");
         String token = BearerTokenExtractor.extract(ctx);
         if (token == null || token.isBlank()) {
             LOGGER.warn("Missing or invalid Authorization header");
@@ -25,15 +30,17 @@ public record KeycloakJwtAuthHandler(JWTAuth jwtAuth) implements AuthenticationH
         jwtAuth.authenticate(new JsonObject().put("token", token))
                 .onComplete(ar -> {
                     if (ar.succeeded()) {
-                        LOGGER.debug("auth successful for Keycloak JWT");
+                        LOGGER.debug("Authentication successful for AAA JWT");
                         ctx.setUser(ar.result());
                         ctx.put("auth_failed", false);
                         ctx.next();
                     } else {
                         LOGGER.warn("Auth failed: {}", ar.cause().getMessage());
-                        ctx.put("auth_error", ar.cause().getMessage());
+                        // do NOT call ctx.fail(ar.cause());
                         ctx.put("auth_failed", true);
+                        ctx.put("auth_error", ar.cause().getMessage());
                     }
                 });
     }
+
 }
