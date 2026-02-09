7d4f690020fc8257b0844be87854452ee5f1db31
Ankit Singh <101859999+ankitmashu@users.noreply.github.com>
2026-02-09 16:54:55 +0530

    redis connection code

---
+++ src/main/java/org/cdpg/dx/database/redis/service/package-info.java
@@ -0,0 +1,5 @@
+@ModuleGen(groupPackage = "org.cdpg.dx.database.redis.service",
+    name = "org-cdpg-dx-database-redis-service",useFutures = true)
+package org.cdpg.dx.database.redis.service;
+
+import io.vertx.codegen.annotations.ModuleGen;
